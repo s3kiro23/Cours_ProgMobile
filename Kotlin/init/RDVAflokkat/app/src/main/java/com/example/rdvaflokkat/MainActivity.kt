15fca939418c7271efc203edd8ce756ab251edb2
Jm <spawn2x@protonmail.com>
2022-09-01 23:39:52 +0200

    update RDV

--- Kotlin/init/RDVAflokkat/app/src/main/java/com/example/rdvaflokkat/MainActivity.kt
+++ Kotlin/init/RDVAflokkat/app/src/main/java/com/example/rdvaflokkat/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.rdvaflokkat
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.util.Log
+import android.view.View
+import android.widget.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,17 +15,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val spinner: Spinner = findViewById(R.id.spinner)
-// Create an ArrayAdapter using the string array and a default spinner layout
+        val selection = "Ludovic"
+        val cal = Calendar.getInstance()
+        val editTextDate: EditText = findViewById(R.id.editTextDate)
+
         ArrayAdapter.createFromResource(
             this,
             R.array.Conseillers,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
+
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
+
             spinner.adapter = adapter
+            spinner.setSelection(1);
+
         }
+
+        fun updateDateInView() {
+            val myFormat = "dd/MM/yyyy"
+            val sdf = SimpleDateFormat(myFormat, Locale.FRENCH)
+            editTextDate.hint = sdf.format(cal.time)
+        }
+
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+
+        editTextDate.setOnClickListener {
+            DatePickerDialog(
+                this@MainActivity,
+                dateSetListener,
+                // set DatePickerDialog to point to today's date when it loads up
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
         supportActionBar?.title = "RDV Aflokkat"
     }
+
 }
